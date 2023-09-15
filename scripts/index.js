87eb6dff3fe78c47029ad799530496a7d33753aa
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-09-15 17:06:46 -0300

    Revert "crear funcion alternadora"
    This reverts commit 41847fe5fb1c07ee7fb4a30e40ed6d90f166b4d5.

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,10 @@
         navButton = d.querySelector('.navbar button'),
         navMenu = d.querySelector('.navbar .nav');
 /* Funciones */
-    function toggle(target, btn, className, [a,b]){
-        target.classList.toggle(className) ?
-        btn.classList.replace(a,b) :
-        btn.classList.replace(b,a)
-    }
+
 
 /* Eventos */
     // Menu Hamburguesa
-    navButton.onclick = toggle(navMenu, navButton, 'active', ['icon-toggle', 'icon-close'])
+    navButton.onclick = function(){
+        navMenu.classList.toggle('active');
+    }
