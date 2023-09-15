2e63980f465d40aa8554351587140e519b5eb347
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-09-15 17:12:43 -0300

    corregir implementacion de funcion alternadora

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,12 @@
         navButton = d.querySelector('.navbar button'),
         navMenu = d.querySelector('.navbar .nav');
 /* Funciones */
-
+    function toggle(target, btn, className, [a,b]){
+        target.classList.toggle(className) ?
+        btn.classList.replace(a,b) :
+        btn.classList.replace(b,a)
+    }
 
 /* Eventos */
     // Menu Hamburguesa
-    navButton.onclick = function(){
-        navMenu.classList.toggle('active');
-    }
+    navButton.onclick = toggle(navMenu, navButton, 'active', ['icon-toggle', 'icon-close'])
