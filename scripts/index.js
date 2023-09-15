2a8e52b4fb5d05e1a1eb61a3724ac6cdadd37aee
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-09-15 15:28:36 -0300

    agregar logica de apertura menu hamburguesa

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 /* Declaraciones */
-
-
+    const 
+        d = document,
+        // Menu Hamburguesa
+        navButton = d.querySelector('.navbar button'),
+        navMenu = d.querySelector('.navbar .nav');
 /* Funciones */
 
 
 /* Eventos */
-
+    // Menu Hamburguesa
+    navButton.onclick = function(){
+        navMenu.classList.toggle('active');
+    }
